3a5e807786def9f721c20460c3877db162a71e71
hduquinha <henriqueduquis1@gmail.com>
2025-05-20 15:34:18 -0300

    fixx: melhoras no amibiente em nuvem

--- back-end/db/database.js
+++ back-end/db/database.js
@@ -9,21 +9,20 @@ let db = null; // Variável para armazenar a instância do Sequelize
 const startDb = async () => {
     if (!db) {
         const connection = await mysql.createConnection({
-            host: "localhost",
+            host: "db", // Nome do serviço no docker-compose
             user: "root",
-            password: ""
-        });
+            password: "root" // Senha definida no docker-compose
+});
         
         // Cria o banco de dados se não existir
         await connection.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
         
         // Inicializa o Sequelize e armazena a instância na variável db
-        db = new Sequelize(DB_NAME, 'root', '', {
-            host: 'localhost',
+        db = new Sequelize(DB_NAME, 'root', 'root', { // Adicione a senha
+            host: 'db', // Nome do serviço no docker-compose
             dialect: 'mysql',
             logging: false 
-   
-        });
+});
         
     }
     
