1607d84741a39c7b92f9d731e2c2dde60e8fb09a
Mariana Graziele Ongarato Toazza <lauroliberatonneto@gmail.com>
2025-11-18 11:33:31 -0300

    fix: arrumando o tamanhao do grafico

--- front-end/src/Pages/home/Componentes_Home/grafic.jsx
+++ front-end/src/Pages/home/Componentes_Home/grafic.jsx
@@ -115,7 +115,7 @@ const ReportsChart = () => {
   return (
     <div
       ref={ref}
-      style={{ width: '100%', maxWidth: '60rem', margin: '0 auto', padding: '20px', backgroundColor: '#f4f4f4', borderRadius: '12px' }}
+      style={{ width: '100%', maxWidth: '70rem', margin: '0 auto', padding: '20px', backgroundColor: '#f4f4f4', borderRadius: '12px' }}
     >
       <h3 style={{ textAlign: 'center', marginBottom: '20px', color: '#6F6F6F' }}>Relatórios gerados por mês</h3>
       <Bar data={data} options={options} />
